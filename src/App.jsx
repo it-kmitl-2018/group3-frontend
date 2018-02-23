5e602587aa1d9f0e50d0ace7c6d58638a28b96b3
chin8628 <boon.8628@gmail.com>
2018-02-23 23:50:34 +0700

    [Add] Add home component with a example code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import logo from './logo.svg'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
 import './App.css'
 
 const App = () =>
   (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <h1 className="App-title">E-TAX</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={ Home } />
+        </Switch>
+      </BrowserRouter>
     </div>
   )
-
 
 export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  constructor() {
+    super()
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is a example page.</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
