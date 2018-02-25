8c9e68cfd6ef6f09758bc3d4a75e6990ba25e5d9
chin8628 <boon.8628@gmail.com>
2018-02-25 13:29:44 +0700

    [Update] Add the router into App.jsx and move old code to the Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
+import EtaxForm from "../EtaxForm/EtaxForm"
 
 class Home extends Component {
   state = {}
 
   render() {
     return (
-      <div>
-        <h1>This is a example page.</h1>
+      <div className="container">
+        <div className="py-5 text-center">
+          <h2>E-TAX Invoice XML Generator</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam amet
+            quos eos id officia voluptatem quae eveniet! Natus, non a.
+          </p>
+          <EtaxForm />
+        </div>
       </div>
     )
   }
