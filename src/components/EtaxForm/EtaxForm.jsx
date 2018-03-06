5d45edb418fcebb8c94d10dc1f479fb1cf2f0c76
Chiibi <Chiibi_jame@hotmail.com>
2018-03-06 11:58:19 +0700

    [Add] PostalTrade API call

--- src/components/EtaxForm/EtaxForm.jsx
+++ src/components/EtaxForm/EtaxForm.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import EqualLayoutForm from "./components/EqualLayoutForm"
+import Api from "../../services/api"
+import Button from "material-ui/Button"
 import {
   addressFieldsState,
   addressFields,
@@ -21,12 +23,23 @@ class EtaxForm extends React.Component {
     })
   }
 
+  submitForm = () => {
+    Api.post("/address", this.state)
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
-      <EqualLayoutForm
-        fields={ addressFields }
-        handleChange={ this.handleChange }
-      />
+      <div>
+        <EqualLayoutForm
+          fields={ addressFields }
+          handleChange={ this.handleChange }
+        />
+        <Button onClick={this.submitForm} variant="raised" color="primary">
+          Submit
+        </Button>
+      </div>
     )
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
+console.log(process.env.REACT_APP_API_URL);
+
 export default axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 2000,
 })
