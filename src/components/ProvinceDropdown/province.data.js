2831cf1d7cb9a5d1da161afe7ce4cd932c541da6
chin8628 <boonyarith.pir@gmail.com>
2018-04-05 21:10:40 +0700

    [Update] Change province data type

---
+++ src/components/ProvinceDropdown/province.data.js
@@ -0,0 +1,392 @@
+export default [
+  {
+    code: "TH-10",
+    title_th: "กรุงเทพมหานคร",
+    type: "เขตปกครองพิเศษ",
+  },
+  {
+    code: "TH-11",
+    title_th: "สมุทรปราการ",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-12",
+    title_th: "นนทบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-13",
+    title_th: "ปทุมธานี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-14",
+    title_th: "พระนครศรีอยุธยา",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-15",
+    title_th: "อ่างทอง",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-16",
+    title_th: "ลพบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-17",
+    title_th: "สิงห์บุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-18",
+    title_th: "ชัยนาท",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-19",
+    title_th: "สระบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-20",
+    title_th: "ชลบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-21",
+    title_th: "ระยอง",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-22",
+    title_th: "จันทบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-23",
+    title_th: "ตราด",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-24",
+    title_th: "ฉะเชิงเทรา",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-25",
+    title_th: "ปราจีนบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-26",
+    title_th: "นครนายก",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-27",
+    title_th: "สระแก้ว",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-30",
+    title_th: "นครราชสีมา",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-31",
+    title_th: "บุรีรัมย์",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-32",
+    title_th: "สุรินทร์",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-33",
+    title_th: "ศรีสะเกษ",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-34",
+    title_th: "อุบลราชธานี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-35",
+    title_th: "ยโสธร",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-36",
+    title_th: "ชัยภูมิ",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-37",
+    title_th: "อำนาจเจริญ",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-38",
+    title_th: "บึงกาฬ",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-39",
+    title_th: "หนองบัวลำภู",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-40",
+    title_th: "ขอนแก่น",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-41",
+    title_th: "อุดรธานี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-42",
+    title_th: "เลย",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-43",
+    title_th: "หนองคาย",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-44",
+    title_th: "มหาสารคาม",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-45",
+    title_th: "ร้อยเอ็ด",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-46",
+    title_th: "กาฬสินธุ์",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-47",
+    title_th: "สกลนคร",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-48",
+    title_th: "นครพนม",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-49",
+    title_th: "มุกดาหาร",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-50",
+    title_th: "เชียงใหม่",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-51",
+    title_th: "ลำพูน",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-52",
+    title_th: "ลำปาง",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-53",
+    title_th: "อุตรดิตถ์",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-54",
+    title_th: "แพร่",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-55",
+    title_th: "น่าน",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-56",
+    title_th: "พะเยา",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-57",
+    title_th: "เชียงราย",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-58",
+    title_th: "แม่ฮ่องสอน",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-60",
+    title_th: "นครสวรรค์",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-61",
+    title_th: "อุทัยธานี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-62",
+    title_th: "กำแพงเพชร",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-63",
+    title_th: "ตาก",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-64",
+    title_th: "สุโขทัย",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-65",
+    title_th: "พิษณุโลก",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-66",
+    title_th: "พิจิตร",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-67",
+    title_th: "เพชรบูรณ์",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-70",
+    title_th: "ราชบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-71",
+    title_th: "กาญจนบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-72",
+    title_th: "สุพรรณบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-73",
+    title_th: "นครปฐม",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-74",
+    title_th: "สมุทรสาคร",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-75",
+    title_th: "สมุทรสงคราม",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-76",
+    title_th: "เพชรบุรี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-77",
+    title_th: "ประจวบคีรีขันธ์",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-80",
+    title_th: "นครศรีธรรมราช",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-81",
+    title_th: "กระบี่",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-82",
+    title_th: "พังงา",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-83",
+    title_th: "ภูเก็ต",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-84",
+    title_th: "สุราษฎร์ธานี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-85",
+    title_th: "ระนอง",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-86",
+    title_th: "ชุมพร",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-90",
+    title_th: "สงขลา",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-91",
+    title_th: "สตูล",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-92",
+    title_th: "ตรัง",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-93",
+    title_th: "พัทลุง",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-94",
+    title_th: "ปัตตานี",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-95",
+    title_th: "ยะลา",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-96",
+    title_th: "นราธิวาส",
+    type: "จังหวัด",
+  },
+  {
+    code: "TH-S",
+    title_th: "พัทยา",
+    type: "เขตปกครองส่วนท้องถิ่นรูปแบบพิเศษ",
+  },
+]
