2d98897652facdb8e02d50e3cb99c7837635a757
chin8628 <boon.8628@hotmail.com>
2018-05-06 19:55:26 +0700

    [Update] Remove unnecessary semicolon

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-console.log(process.env.REACT_APP_API_URL);
+console.log(process.env.REACT_APP_API_URL)
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API_URL,
